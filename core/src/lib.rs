a613e11bd52a6f7e04354415125ce82114468d7d
Charles d'Avernas <charles.davernas@neuroglia.io>
2025-01-15 17:43:23 +0100

    Add a new `common` field to all `TaskDefinitions`
    Signed-off-by: Charles d'Avernas <charles.davernas@neuroglia.io>

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -16,14 +16,20 @@ mod unit_tests {
         let title = Some("fake-title".to_string());
         let summary = Some("fake-summary".to_string());
         let document = WorkflowDefinitionMetadata::new(namespace, name, version, title.clone(), summary.clone(), None);
+        let mut call_task = CallTaskDefinition::new("http", None, Some(true));
+        call_task.common.then = Some("continue".to_string());
+        let do_task = DoTaskDefinition::new(Map::from(vec![("set".to_string(), TaskDefinition::Wait(WaitTaskDefinition::new(OneOfDurationOrIso8601Expression::Duration(Duration::from_milliseconds(200)))))]));
         let mut workflow = WorkflowDefinition::new(document);
         workflow.do_ = Map::new();
-        workflow.do_.add("callTask".to_string(), TaskDefinition::Call(CallTaskDefinition::new("http", None, Some(true))));
-        workflow.do_.add("doTask".to_string(), TaskDefinition::Do(DoTaskDefinition::new(Map::from(vec![("set".to_string(), TaskDefinition::Wait(WaitTaskDefinition::new(OneOfDurationOrIso8601Expression::Duration(Duration::default()))))]))));
+        workflow.do_.add("callTask".to_string(), TaskDefinition::Call(call_task));
+        workflow.do_.add("doTask".to_string(), TaskDefinition::Do(do_task));
         let json_serialization_result = serde_json::to_string_pretty(&workflow);
         let yaml_serialization_result = serde_yaml::to_string(&workflow);
         assert!(json_serialization_result.is_ok(), "JSON Serialization failed: {:?}", json_serialization_result.err());
         assert!(yaml_serialization_result.is_ok(), "YAML Serialization failed: {:?}", yaml_serialization_result.err());
+        if let Result::Ok(yaml) = yaml_serialization_result{
+            println!("{}", yaml)
+        }
         assert_eq!(workflow.document.namespace, namespace);
         assert_eq!(workflow.document.name, name);
         assert_eq!(workflow.document.version, version);
